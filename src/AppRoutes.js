8c0607aca1cbf9667b7c015df2afe0c74798af4e
Ashutosh Kumar <90816826+Ashutosh27488674@users.noreply.github.com>
2022-06-07 16:51:36 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export const AppRoutes = () => {
     return (
+        <div>
+        <a href="/">Index Page</a>
+        <a href="/home">Index Page</a>
+        <a href="/">Index Page</a>
+        
+        </div>
 
     )
 }

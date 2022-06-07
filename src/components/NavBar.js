249bb74579ed79b2e98520a2b821580f1f96f8f1
Ashutosh Kumar <90816826+Ashutosh27488674@users.noreply.github.com>
2022-06-07 17:09:24 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ export const NavBar = () =>{
     return(
         <nav>
         <ul>
-        <li class="index-link" ><a href="/index">Index</a></li>
-        <li class="home-link" ><a href="/home">Home Link</a></li>
+        <li class="index-link" ><a href="../Pages/Index.js">Index</a></li>
+        <li class="home-link" ><a href="../Pages/Home.js">Home Link</a></li>
         
         </ul>
         </nav>
